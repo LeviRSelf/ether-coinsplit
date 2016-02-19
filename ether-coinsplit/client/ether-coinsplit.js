9d66cb3155b9fbb58fc4a82fbc9f4564ec6a16f6
LeviRSelf <levi.r.self@gmail.com>
2016-02-19 01:04:42 -0600

    UX Improvements

--- ether-coinsplit/client/ether-coinsplit.js
+++ ether-coinsplit/client/ether-coinsplit.js
@@ -3,27 +3,17 @@ if (Meteor.isClient) {
   Recipients = new Meteor.Collection("recipients", {connection: null});
 
   for (i=1; i<=10; i++) {
-    var x = Recipients.findOne({number: i});
-    if (x == undefined) {
-      Recipients.insert({
-        number: i,
-        address: '',
-        placeholder: 'Recipient Address '+i,
-        share: 0
-      });
-    }
+    Recipients.insert({
+      number: i,
+      address: '',
+      share: 0
+    });
   }
 
-  Session.set("notice", undefined);
-  Session.set("isConnected", web3.isConnected());
-  Session.set("compiledContract", undefined);
-  Session.set("total", 0);
-
-  Template.notice.helpers({
-    notice: function(){
-      return Session.get("notice");
-    }
-  })
+  Session.set("sourcebox","Contract Source");
+  Session.set("varbox","Contract Variables");
+  Session.set("abibox","ABI");
+  Session.set("resultbox","Results");
 
   Template.body.checkConnection = function(){
     Meteor.setTimeout(function(){
@@ -53,44 +43,90 @@ if (Meteor.isClient) {
     total: function() {
       var s=0;
       Recipients.find({}).map( function(doc) {s += doc.share} );
-      document.getElementById("total").value = s;
+      Session.set("total",s);
+      return s
+    },
+    notice: function(){
+      return Session.get("notice");
+    },
+    sourcebox: function(){
+      return Session.get("sourcebox")
+    },
+    varbox: function(){
+      return Session.get("varbox")
+    },
+    abibox: function(){
+      return Session.get("abibox")
+    },
+    resultbox: function(){
+      return Session.get("resultbox")
     }
   })
 
   Template.recipient.events({
     'change input': function(event) {
-      Recipients.update(this._id, {
-        $set: {share: parseFloat(event.target.value)}
-      });
+      if (event.target.type == 'number') {
+        Recipients.update(this._id, {
+          $set: {share: parseFloat(event.target.value)}
+        });
+      }
     }
   });
   Template.controlbuttons.events({
     'click .generate-contract-btn': function () {
-      var splitterSource = 'contract splitter {    address[2] recipients;   event donation(address _from, uint _amount);    function splitter(address[2] _recipients) public {   recipients = _recipients;  }   function getAddresses() constant returns (address[2]) {   return recipients;  }   function donate() public {    recipients[0].send(msg.value / 2);   recipients[1].send(msg.value / 2);   }  } ';
-      var compiledContract = web3.eth.compile.solidity(splitterSource);
-      Session.set("compiledContract",compiledContract);
-      var abiDefinition = JSON.stringify(compiledContract.splitter.info.abiDefinition);
-      console.log(splitterSource);
-      console.log(compiledContract);
-      console.log(abiDefinition);
+
+      //Validation
+      var addresses = [];
+      var shares = [];
+      for (i=1; i<=10; i++) {
+        var share = Recipients.findOne({number:i}).share;
+        if (share !== 0) {
+          addresses.push(document.getElementById("addr"+i).value);
+          shares.push(share);
+        }
+      }
+
+      if (Math.abs(Session.get("total") - 100) > 0.001) {
+        Session.set("resultbox","Shares must add up to 100.");
+      }
+      else if (!addresses.every(e => web3.isAddress(e))) {
+        Session.set("resultbox","One or more addresses is not a valid ethereum address.");
+      }
+      else {
+        Session.set("resultbox","Contract compiled!");
+        var splitterSource = 'contract splitter { address['+addresses.length+'] recipients;  function splitter(address['+addresses.length+'] _recipients) public {   recipients = _recipients;  }   function getAddresses() constant returns (address['+addresses.length+']) {   return recipients;  }   function donate() public {  '
+        for (i=0; i<addresses.length; i++) {
+          splitterSource += 'recipients['+i+'].send(msg.value * '+shares[i]+' * 1000 / 100000); ';
+        }
+        splitterSource += ' }} ';
+        Session.set("sourcebox",splitterSource);
+        var compiledContract = web3.eth.compile.solidity(splitterSource);
+        Session.set("compiledContract",compiledContract);
+        var abiDefinition = JSON.stringify(compiledContract.splitter.info.abiDefinition);
+        Session.set("abibox",abiDefinition);
+        Session.set("recipients",addresses);
+        Session.set("varbox","_recipients:\n\n"+addresses);
+        console.log(splitterSource);
+        console.log(compiledContract);
+        console.log(abiDefinition);
+      }`
 
     },
     'click .deploy-contract-btn': function () {
       var compiledContract = Session.get("compiledContract");
       var splitterContract = web3.eth.contract(compiledContract.splitter.info.abiDefinition);
 
-      var _recipients = ['0x2021cae0729f4319057905478484864c89bd4e2d','0x25f688fcdd634beae8254cc27e9759d400b8ce0d'];
-
+      var _recipients = Session.get("recipients");
       var splitter = splitterContract.new(_recipients,{from:web3.eth.accounts[0], data: Session.get("compiledContract").splitter.code, gas: 3000000}, function(e, contract){
         if(!e) {
 
           if(!contract.address) {
             console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
-            Session.set("notice", "Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+            Session.set("resultbox", "Contract transaction sent!\n\nTransactionHash: " + contract.transactionHash + "\n\nwaiting to be mined...");
 
           } else {
             console.log("Contract mined! Address: " + contract.address);
-            Session.set("notice", "Contract mined! Address: " + contract.address + "       ABI: " + JSON.stringify(contract.abi));
+            Session.set("resultbox", "Contract mined!\n\nAddress: " + contract.address);
           }
 
         }
